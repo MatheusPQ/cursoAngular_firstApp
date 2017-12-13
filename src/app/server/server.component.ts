4f884b87cecf332ccfe5536dea6222ee26524b18
Matheus <matheuspq79@outlook.com.br>
2017-12-13 19:10:07 -0200

    Primeiro commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-server', //A tag html, basicamente
+    templateUrl: './server.component.html' //O arquivo html
+})
+
+export class ServerComponent {
+
+}
